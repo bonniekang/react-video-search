5defe494977b6550933c150f8b25a4d660551978
jihye kang <jihye59kang@gmail.com>
2021-06-21 22:29:07 +0100

    set up axios

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,14 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyD_RyjxEwZT4KDL5dJEmUHqLPFFZwIMpl4';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY,
+    }
+})
+
